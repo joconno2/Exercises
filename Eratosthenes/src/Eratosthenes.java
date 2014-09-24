6e8d3c9f5037045d0d5ccdf2021327b1bbd57325
Jim O'Connor <joconno2@conncoll.edu>
2014-09-24 16:03:47 -0400

    Finished the Sieve
    Finished a Sieve of Eratosthenes implementation exercise

--- Eratosthenes/src/Eratosthenes.java
+++ Eratosthenes/src/Eratosthenes.java
@@ -1,5 +1,8 @@
-import java.util.ArrayList;
-import java.util.List;
+// Jim O'Connor
+//
+// This program determines the number of Primes between 0 
+// and a given number using the Sieve of Eratosthenes
+
 import java.util.Scanner;
 
 
@@ -8,22 +11,34 @@ public class Eratosthenes {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
-		System.out.print("Input a number, to check primality: ");
+		System.out.print("Input a number, to check the number of primes up to that number: ");
 		int number = scanner.nextInt();
 		scanner.close();
 		
-		if(sieve(number))
-			System.out.print("\nThis number is prime.");
-		else
-			System.out.print("\nThis number is not prime.");
+		System.out.print("\nThere are ");
+		sieve(number);
+		System.out.print(" primes between 0 and "+number+".");
 		
 	}
 
-	public static boolean sieve(int number){
-		List<Boolean> list = new ArrayList<Boolean>(number);
+	// Determines the number of primes between 0 and the given value
+	// using the Sieve of Eratosthenes
+	public static void sieve(int number){
+		boolean[] list = new boolean[number + 1];
+		int primes = 0;
 		
-		//To-Do
+		for(int i = 2; i <= number; i++)  // true the whole list
+			list[i] = true;
 		
-		return true;
+		for(int i = 2; i*i <= number; i++)
+			if(list[i])
+				for(int j = i; i*j <= number; j++) // sieve the list
+					list[i*j] = false;
+		
+		for(int i = 0; i < number; i ++) // count the primes in the list post-sieve
+			if(list[i])
+				primes++;
+		
+		System.out.print(primes); // output the number of primes
 	}
 }
